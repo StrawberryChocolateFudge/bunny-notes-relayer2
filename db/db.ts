6657b223db884df9c369ee23c322ab8c1434de67
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-07-06 22:56:12 +0200

    added merkle tree storage

--- db/db.ts
+++ db/db.ts
@@ -1,5 +1,41 @@
-// import { DB } from "https://deno.land/x/sqlite@v3.7.2/mod.ts";
+import { DB } from "https://deno.land/x/sqlite@v3.7.2/mod.ts";
 
-// const db = new DB("relayer.db");
+type StoredTreeTuple = [number, string, string, string];
 
-//NOT YET IMPLEMENTED
+const db = new DB("relayer.db");
+// I want to store the merkle root and the leaves of the tree (commitments) in the database
+// For Bunny Bundles!
+// The root is a string and the  leaves are a json with an array of strings inside
+db.execute(`
+  CREATE TABLE IF NOT EXISTS merkleTrees (
+    id INTEGER PRIMARY KEY,
+    root TEXT NOT NULL UNIQUE,
+    leaves TEXT NOT NULL,
+    network TEXT NOT NULL
+  )
+`);
+
+const addMerkleTree = db.prepareQuery(
+  "INSERT INTO merkleTrees (root, leaves,network) VALUES (:root, :leaves, :network)",
+);
+
+const getMerkleTreeByRoot = db.prepareQuery<StoredTreeTuple>(
+  "SELECT root,leaves,network FROM merkleTrees WHERE root = :root AND network = :network",
+);
+
+export function runAddMerkleTree(
+  root: string,
+  leaves: string[],
+  network: string,
+): boolean {
+  try {
+    addMerkleTree.execute({ root, leaves: JSON.stringify(leaves), network });
+    return true;
+  } catch (_err) {
+    return false;
+  }
+}
+
+export function getMerkleTree(root: string, network: string) {
+  return getMerkleTreeByRoot.first({ root, network });
+}
