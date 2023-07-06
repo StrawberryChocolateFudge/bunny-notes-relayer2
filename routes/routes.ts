8ead7094dee07753a728234d56b9acdc6cc20785
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-07-06 22:56:43 +0200

    added merkle tree upload

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,7 +1,13 @@
 import { Router } from "https://deno.land/x/oak@v12.5.0/mod.ts";
-import { validateWithdrawRequest } from "./validators.ts";
-import { handleWithdraw } from "../web3/web3.ts";
+import {
+  validateBundleFetch,
+  validateBundleUpload,
+  validateWithdrawRequest,
+} from "./validators.ts";
+import { checkBundleExists, handleWithdraw } from "../web3/web3.ts";
 import { EXPIRYTIME, requestMutex } from "./requestMutex.ts";
+import { getMerkleTree, runAddMerkleTree } from "../db/db.ts";
+import { ChainIds } from "../web3/constants.ts";
 
 export const router = new Router();
 
@@ -43,15 +49,62 @@ router.post("/withdraw", async (context) => {
 });
 
 // Will upload a bunny bundle merkle tree
-router.post("/bundle", (context) => {
-  context.response.body = "not implemented";
-  context.response.status = 501;
+router.post("/bundle", async (context) => {
+  const rawBody = context.request.body({ type: "json" });
+  const json = await rawBody.value;
+  const [status, msg] = validateBundleUpload(json);
+
+  if (status === 400) {
+    context.response.body = { message: msg };
+    context.response.status = status;
+    return;
+  }
+
+  const bundleExists = checkBundleExists(json.network as ChainIds, json.root);
+  if (!bundleExists) {
+    context.response.body = { message: "Invalid root. Does not exist." };
+    context.response.status = 400;
+    return;
+  }
+
+  const success = runAddMerkleTree(json.root, json.leaves, json.network);
+
+  if (!success) {
+    context.response.body = { message: "Unable to save merkle tree" };
+    context.response.status = 400;
+    return;
+  }
+
+  context.response.body = { message: "ok" };
+  context.response.status = 200;
 });
 
 // Will return a bunny bundle merkle tree by the root
-router.get("/bundle", (context) => {
-  context.response.body = "not implemented";
-  context.response.status = 501;
+router.get("/bundle", async (context) => {
+  const rawBody = context.request.body({ type: "json" });
+  const json = await rawBody.value;
+  const [status, msg] = validateBundleFetch(json);
+  if (status === 400) {
+    context.response.body = { message: msg };
+    context.response.status = status;
+    return;
+  }
+
+  const row = getMerkleTree(json.root, json.network);
+
+  if (!row) {
+    context.response.body = { message: "Unable to find merkle tree" };
+    context.response.status = 400;
+    return;
+  }
+
+  context.response.body = {
+    message: "ok",
+    root: row[1],
+    leaves: JSON.parse(row[2]),
+    network: row[3],
+  };
+  context.response.status = 200;
 });
 
 // Will return a url to redirect to, for processing the direct debit request
@@ -71,7 +124,3 @@ router.post("processDirectDebit", (context) => {
   context.response.body = "not implemented";
   context.response.status = 501;
 });
-
-function toNoteHex(arg0: any): string {
-  throw new Error("Function not implemented.");
-}
